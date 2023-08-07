c768a26a21c0335ae43047cee8a974b5d4687486
Andrew Barrett <andrew.barrett@peoplestrategy.com>
2023-08-07 12:05:16 -0500

    React Tutorial 1

---
+++ src/Content.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ItemList from "./ItemList";
+
+const Content = ({ items, handleCheck, handleDelete }) => {
+  return (
+    <main>
+      {items.length ? (
+        <ItemList
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+          items={items}
+        />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty</p>
+      )}
+    </main>
+  );
+};
+
+export default Content;
